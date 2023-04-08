a53625bd0d935bb1daec9cc522c3304047f4ff17
ks0510 <kshaikh051999@gmail.com>
2023-04-08 19:42:16 +0530

    Added java program to display welcome

---
+++ DisplayWage.java
@@ -0,0 +1,18 @@
+package wagecalculation;
+/**
+ * 
+ * @author Kaif
+ *
+ */
+
+public class DispalyWageComputation {
+	/**
+	 * Create this method to print Welcome messege
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+	     System.out.println("Welcome to Employee Wage Computation");
+	}
+}
